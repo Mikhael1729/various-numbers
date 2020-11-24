c99503beed700088211627bf6c8a8e5bd6f3cb52
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-11-24 01:06:01 -0400

    Continuing making the social numbers identification algorithm

--- main.py
+++ main.py
@@ -38,25 +38,78 @@ def compute_factors(number):
 
   return factors
 
-def get_sum(factors):
+def compute_sum(factors):
   sum = 0
   for factor in factors:
     sum = sum + factor
 
   return sum
 
-def print_friend_numbers_in_range(start, end):
-  serie = range(start, end)
-  for a in serie:
-    a_factors = compute_factors(a)
-    sum1 = get_sum(a_factors)# Sum of the factors of a number
-    for b in serie:
-      b_factors = compute_factors(b)
-      sum2 = get_sum(b_factors) # Sum of the factors of b number
+def compute_factors_sum(number):
+  factors = compute_factors(number)
+  factors_sum = compute_sum(factors)
 
-      is_a_friend_number = sum1 == b and sum2 == a
+  return factors_sum
+
+def is_perfect_number(number, factors_sum):
+  return True if factors_sum == number else False
+
+def is_friend_number(a, a_factors_sum, b, b_factors_sum):
+  return a_factors_sum == b and b_factors_sum == a
+
+def print_friend_numbers_in_range(start, end):
+  amicable_numbers = []
+  perfect_numbers = []
+  sociable_numbers = []
+
+  serie = list(range(start, end))
+  size = len(serie)
+
+  for i in range(0, len(size)):
+    a = serie[i]
+    a_factors_sum = compute_factors_sum(a)# Sum of the factors of a number
+
+    # Perfect number evaluation.
+    a_is_perfect_number = is_perfect_number(a, a_factors_sum)
+    if a_is_perfect_number:
+      perfect_numbers.append(a)
+      del serie[i]
+      continue
+
+    for j in range(0, len(size)):
+      b = serie[j]
+      b_factors_sum = compute_factors_sum(b) # Sum of the factors of b number
+
+      is_a_friend_number = is_friend_number(a, a_factors_sum, b, b_factors_sum)
       if is_a_friend_number:
-        print(f"Friends: a: {a}, b: {b}")
+        amicable_numbers.append(a)
+        amicable_numbers.append(b)
+        del serie[i]
+        del serie[j]
+        continue
+      # TODO: Coninue writing the step 6
+      elif:
+
+      
+      
+
+
+"""
+- Amicable numbers
+- Perfect numbers
+- Other sociable numbers of n + 2 periods
+
+A = [a, b], # Amicable numbers
+P = [], # Perfect numbers
+S = [[3], [4], [5], [n]]
+
+
+P = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+1. Tomo el primero número 
+2. 
+2. Empi
+"""
 
 n = 3
 start, end = 10**(n-1), 10**n
